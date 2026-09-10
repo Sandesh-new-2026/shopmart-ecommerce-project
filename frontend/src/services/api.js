90afabdd9419ed9c6c29e60dd994f1fca7e0075d
Sandesh-new-2026 <sdbhadane1@gmail.com>
2026-09-10 23:39:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const configuredApiUrl =
-  import.meta.env.VITE_API_URL ||
-  "https://shopmart-backend-ifz0dzxny-sandesh-react-projects.vercel.app";
-const apiRoot = configuredApiUrl.replace(/\/+$/, "").replace(/\/api$/, "");
 const api = axios.create({
-  baseURL: `${apiRoot}/api`,
+  baseURL: "/api",
 });
 
 api.interceptors.request.use((config) => {
