38ba9542f0e5ca27003d7f0a77cb9c25a7a42a4d
Sandesh-new-2026 <sdbhadane1@gmail.com>
2026-09-10 23:40:42 +0530

    Add proxy configuration for API requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://shopmart-backend-ifz0dzxny-sandesh-react-projects.vercel.app",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
